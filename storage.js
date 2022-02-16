04f52d21c236a9a3d2ca0bac2f0ffd721f3f3c08
Serafim <7620226@gmail.com>
2022-02-16 05:17:03 +0300

    feat: add records

--- storage.js
+++ storage.js
@@ -7,11 +7,36 @@ function setLocalStorage() {
   } else if (parseInt(localStorage.getItem("score")) < score) {
     localStorage.setItem("score", score);
   }
+  if (localStorage.getItem("records")) {
+    if (score != 0) {
+      let recordsArray = localStorage
+        .getItem("records")
+        .split(" ")
+        .filter((item) => item != 0)
+        .slice(0, 10)
+        .join(" ");
+      localStorage.setItem("records", `${recordsArray} ${score}`);
+    }
+  } else {
+    localStorage.setItem("records", score);
+  }
 }
 
 function getLocalStorage() {
   if (localStorage.getItem("score")) {
     const storageScore = localStorage.getItem("score");
     document.querySelector("#max").textContent = storageScore;
+  }
+  if (localStorage.getItem("records")) {
+    let arr = localStorage
+      .getItem("records")
+      .split(" ")
+      .filter((item) => item != 0)
+      .sort((a, b) => b - a);
+    let counter = 0;
+    for (let item of document.querySelectorAll("#r")) {
+      item.textContent = arr[counter];
+      counter++;
+    }
   }
 }
